fbbe7fefd1138b9de3dfac7bbc9ad7f62252f42f
jerrykuo7727 <jerrykuo7727@gmail.com>
2016-07-29 18:22:29 +0800

    Build method to search shortest path

--- knight_moves.rb
+++ knight_moves.rb
@@ -1,29 +1,62 @@
+class Position
+  attr_reader :position, :prev_move
+
+  def initialize(pos, prev=nil)
+    @position = pos
+    @prev_move = prev
+  end
+
+  def next_moves
+    moves = []
+    first = [2, 2, 1, -1, -2, -2, -1, 1]
+    second = [1, -1, -2, -2, -1, 1, 2, 2]
+    8.times do
+      curr = @position.clone
+      curr[0] += first.shift
+      curr[1] += second.shift
+      if @prev_move == nil
+        moves << curr unless out_of_board?(curr)
+      elsif @prev_move.position != curr
+        moves << curr unless out_of_board?(curr)
+      end
+    end
+    moves
+  end
+
+  def out_of_board?(pos)
+    if (0..7).include?(pos[0]) && (0..7).include?(pos[1])
+      return false
+    else
+      return true
+    end
+  end
+end
+
 def knight_moves(start, goal)
-
-end
-
-def all_moves(start)
-  moves = []
-  first = [2, 2, 1, -1, -2, -2, -1, 1]
-  second = [1, -1, -2, -2, -1, 1, 2, 2]
-  8.times do
-    curr = start.clone
-    puts "curr: #{curr}"
-    curr[0] += first.shift
-    curr[1] += second.shift
-    puts "after: #{curr[0]},#{curr[1]}"
-    moves << curr unless out_of_board?(curr)
+  curr = Position.new(start)
+  queue = [curr]
+  until curr.position == goal || queue.empty?
+    curr = queue.shift
+    curr.next_moves.each do |move|
+      next_pos = Position.new(move, curr)
+      queue << next_pos
+    end
   end
-  moves
-end
 
-def out_of_board?(place)
-  if (0..7).include?(place[0]) && (0..7).include?(place[1])
-    return false
-  else
-    return true
+  path = []
+  count = 0
+  until curr == nil
+    path << curr.position
+    count += 1
+    curr = curr.prev_move
+  end
+
+  puts "You made it in #{count} moves! Here's your path:"
+  until path.empty?
+    puts path.pop.to_s
   end
 end
 
-
-p all_moves([3,3])
+knight_moves([0,0],[1,2])
+knight_moves([0,0],[3,3])
+knight_moves([3,3],[0,0])
